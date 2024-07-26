36fc69bd9bd6471e57e340e764bc733629297ef8
mohdArif01 <mohdarif.14011@gmail.com>
2024-07-26 23:33:42 +0530

    User fetching created with routes and controller setup

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -43,3 +43,25 @@ export const sendMessage = async (req, res) => {
         res.status(500).json({ error: "Internal Server Error" })
     }
 }
+
+export const getMessage = async (req, res) => {
+    try {
+
+        const {id: userToChatId} = req.params;
+        const senderId = req.user._id;
+
+        const conversation = await Conversation.findOne({
+            participants: { $all: [senderId, userToChatId] },
+        }).populate("messages"); // NOT REFERENCE BUT ACTUAL MESSAFGES
+
+        if (!conversation) return res.status(200).json([]);
+
+        const messages = conversation.messages;
+
+        res.status(200).json(conversation.messages)
+
+    } catch (error) {
+        console.log("Error in getMessage controller:", error.message)
+        res.status(500).json({ error: "Internal Server Error" })
+    }
+}

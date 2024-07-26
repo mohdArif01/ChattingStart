a6eefdb8c5906cd303d646bce79eb32b4bc7567b
mohdArif01 <mohdarif.14011@gmail.com>
2024-07-27 00:45:47 +0530

    Login and SignUI completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './App.css'
 
 import Login from './pages/login/Login'
+import SignUp from './pages/SignUp/signup'
+import Home from './pages/home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +11,7 @@ function App() {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
 
-      <Login />
+      <Home />
 
     </div>
   )
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-100 bg-clip-padding '>
+      <Sidebar />
+      <Message /> 
     </div>
   )
 }

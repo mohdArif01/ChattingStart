d1a601d749d7630f821d61e0f828f7dc922cccef
mohdArif01 <mohdarif.14011@gmail.com>
2024-07-27 19:37:17 +0530

    Setup of Login Functio\nality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,26 @@
 import { useState } from 'react'
+import {Navigate, Route, Routes} from 'react-router-dom'
 import './App.css'
 
 import Login from './pages/login/Login'
 import SignUp from './pages/SignUp/signup'
 import Home from './pages/home/Home'
+import { Toaster } from 'react-hot-toast'
+import { useAuthContext } from './context/AuthContext'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const {authUser} = useAuthContext()
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
 
-      <Home />
+      <Routes>
+        <Route path='/' element={authUser ? <Home/> : <Navigate to='/login' />} />
+        <Route path='/login' element={authUser ? <Navigate to='/' /> : <Login/>} />
+        <Route path='/signup' element={ authUser ? <Navigate to='/' /> : <SignUp/>} />
+      </Routes>
+      <Toaster />
 
     </div>
   )

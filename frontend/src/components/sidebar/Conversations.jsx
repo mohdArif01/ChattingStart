4f7b0e85b17b0948c7fe439e8d855b1b8de0cafd
mohdArif01 <mohdarif.14011@gmail.com>
2024-07-28 21:37:52 +0530

    Implements the search feature and getting message from database

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import Conversation from './Conversation'
+import useGetConversation from '../../hooks/useGetConversation';
 
 const Conversations = () => {
+
+  const {loading, conversations} = useGetConversation();
   return (
     <div className='py-0 flex flex-col overflow-auto' >
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
+
+      {conversations.map((conversation, idx) => (
+        <Conversation 
+        key={conversation._id}
+        conversation = {conversation}
+        lastIdx={idx === conversation.lenght-1}
+         />
+      ))}
+
+
+      {loading  ? <span className='loading loading-spinner' ></span> : null }
     </div>
   )
 }

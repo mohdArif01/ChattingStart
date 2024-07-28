ef72ab3ee0eabbfab87202223c44fc2e6077e8e6
mohdArif01 <mohdarif.14011@gmail.com>
2024-07-29 00:55:05 +0530

    small Changes

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,15 @@ import cookieParser from 'cookie-parser'
 import authRoutes from './routes/auth.routes.js'
 import messageRoutes from './routes/message.routes.js'
 import userRoutes from './routes/user.routes.js'
+import path from 'path'
 
 import mongoConnect from './db/mongoConnect.js'
 import { app, server } from './socket/socket.js'
 
 // const app = express()
 const PORT = process.env.PORT || 5000
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -21,10 +24,13 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
 
-// app.get("/", (req, res) => {
-//     // root route http://localhost:5000/
-//     res.send("Hello World!")
-// })
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
+
+
 
 
 

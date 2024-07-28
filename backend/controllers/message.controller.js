9a1dd5469ed48d3b72f3d3c2ee76ef61b15fca21
mohdArif01 <mohdarif.14011@gmail.com>
2024-07-28 23:29:39 +0530

    minor changes

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from '../models/conversation.model.js'
 import Message from '../models/message.model.js'
+import { getReceiverSocketId, io } from '../socket/socket.js';
 
 export const sendMessage = async (req, res) => {
     try {
@@ -27,15 +28,22 @@ export const sendMessage = async (req, res) => {
             conversation.messages.push(newMessage._id)
         }
 
-        // SOCKET IO
-
+        
         // await conversation.save()
         // await newMessage.save()
-
+        
         // This will run in Parallel
         await Promise.all([conversation.save(), newMessage.save()])
-
+        
         res.status(201).json(newMessage)
+
+        // SOCKET IO
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if (receiverSocketId) {
+            // io.to(<socket_id>).emit() used to send events to specific client
+            io.to(receiverSocketId).emit("newMessage", newMessage)
+        }
+
 
     }
     catch(error){
